a78ce66b1fe2340b6226e802a2d6c815f22877d5
rickypazd <rickypazd@hotmail.com>
2022-10-17 20:27:57 -0400

    Update App.java

--- ProyectoFinal-CheckIn/WebApi/src/main/java/App.java
+++ ProyectoFinal-CheckIn/WebApi/src/main/java/App.java
@@ -1,22 +1,6 @@
 public class App {
 
   public static void main(String[] args) {
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
-    System.out.println("TEST DE CAMBIOS INTEGRATION TEST");
     ExtensionsInfrastructure.AddInfraestructure();
     ExtensionsWebApi.AddControllers();
   }
